811d2ed0d86f7d9192a43d557b896ac8dac3ebb2
Jean <jeanjyjh@gmail.com>
2024-09-22 20:09:27 +1200

    Update router configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,64 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./components/layout";
+import Home from "./routes/home";
+import Profile from "./routes/profile";
+import Login from "./routes/login";
+import CreateAccount from "./routes/create-account";
+import { createGlobalStyle } from "styled-components";
+import { reset } from "styled-reset";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/create-account",
+    element: <CreateAccount />,
+  },
+]);
+
+const GlobalStyle = createGlobalStyle`
+  ${reset};
+  * {   
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: #15202B;
+    color: white;
+    font-family: 
+      -apple-system, 
+      BlinkMacSystemFont, 
+      "Segoe UI", 
+      Roboto, 
+      "Helvetica Neue", 
+      Arial, 
+      sans-serif;
+  }
+`;
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <GlobalStyle />
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
